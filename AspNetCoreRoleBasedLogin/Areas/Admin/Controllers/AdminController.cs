2b0209b5d506f23b2f668774fd6e2cca8db1fd84
Manish Kumar <manish.kumar@csharptek.com>
2022-07-12 18:13:57 +0530

    Add project files.

---
+++ AspNetCoreRoleBasedLogin/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreRoleBasedLogin.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Authorize(Policy = "AdminPolicy")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult UserList()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
---
+++ AspNetCoreRoleBasedLogin/Areas/User/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreRoleBasedLogin.Areas.User.Controllers
+{
+    [Area("User")]
+    [Authorize(Policy = "UserPolicy")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
